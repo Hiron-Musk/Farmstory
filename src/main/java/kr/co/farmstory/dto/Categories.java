ff8d6fa7e7ad5a500747b855f87c211ef856737e
Amobu0 <kdehgogo104@gmail.com>
2024-04-02 16:33:08 +0900

    Merge pull request

---
+++ src/main/java/kr/co/farmstory/dto/Categories.java
@@ -0,0 +1,18 @@
+package kr.co.farmstory.dto;
+
+import lombok.*;
+
+/**
+ * 24/4/2수정
+ */
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Categories {
+    private int cateNo;
+    private String cateName;
+    private int catNo;
+}
